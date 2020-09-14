21a8957fe2d863d1d67ebd421fc068671266f82c
Jeff Moser <jeff@bitsalt.com>
2020-09-14 15:16:23 -0400

    Added eureka server to project files

---
+++ eureka-server/src/test/java/com/bitsalt/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitsalt.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
